c70aefc3fa6492ee008873391007bb2904daaa00
Andersen Fan <as181920@gmail.com>
2022-06-13 22:09:29 +0800

    Handle sms sign/template apis

--- lib/aliyun_sms/api_client.rb
+++ lib/aliyun_sms/api_client.rb
@@ -10,25 +10,59 @@ module AliyunSms
     end
 
     def send_message(phone_numbers, template_code, template_param, options = {})
-      request_params = {
-        AccessKeyId: access_key_id,
-        Timestamp: Time.now.utc.strftime("%FT%TZ"),
-        SignatureMethod: "HMAC-SHA1",
-        SignatureVersion: "1.0",
-        SignatureNonce: Time.now.utc.strftime("%Y%m%d%H%M%S%L"),
+      request_params = common_params(options:).merge(
         Action: "SendSms",
-        Version: "2017-05-25",
-        Format: "JSON",
-        RegionId: (options[:region_id] || "cn-hangzhou"),
         PhoneNumbers: phone_numbers,
         SignName: (options[:sign_name] || ""),
         TemplateCode: template_code,
         TemplateParam: (template_param.is_a?(String) ? template_param : template_param.to_json)
-      }.tap { |params| params[:Signature] = sign(params) }
+      ).tap { |params| params[:Signature] = sign(params) }
       connection.post("/", request_params)
         .tap { |resp| AliyunSms.logger.info "#{self.class.name} send_message response(#{resp.status}): #{resp.body.squish}" }
         .then { |resp| JSON.parse(resp.body) }
       # Hash.from_xml(resp.body)["SendSmsResponse"]
+    end
+
+    def query_sign(name, options: {})
+      request_params = common_params(options:).merge(
+        Action: "QuerySmsSign",
+        SignName: name
+      ).tap { |params| params[:Signature] = sign(params) }
+      connection.post("/", request_params)
+        .tap { |resp| AliyunSms.logger.info "#{self.class.name} query_sign response(#{resp.status}): #{resp.body.squish}" }
+        .then { |resp| JSON.parse(resp.body) }
+    end
+
+    def query_sign_list(page: 1, per: 10, options: {})
+      request_params = common_params(options:).merge(
+        Action: "QuerySmsSignList",
+        PageIndex: page,
+        PageSize: per
+      ).tap { |params| params[:Signature] = sign(params) }
+      connection.post("/", request_params)
+        .tap { |resp| AliyunSms.logger.info "#{self.class.name} query_sign_list response(#{resp.status}): #{resp.body.squish}" }
+        .then { |resp| JSON.parse(resp.body) }
+    end
+
+    def query_template(template_code, options: {})
+      request_params = common_params(options:).merge(
+        Action: "QuerySmsTemplate",
+        TemplateCode: template_code
+      ).tap { |params| params[:Signature] = sign(params) }
+      connection.post("/", request_params)
+        .tap { |resp| AliyunSms.logger.info "#{self.class.name} query_template response(#{resp.status}): #{resp.body.squish}" }
+        .then { |resp| JSON.parse(resp.body) }
+    end
+
+    def query_template_list(page: 1, per: 10, options: {})
+      request_params = common_params(options:).merge(
+        Action: "QuerySmsTemplateList",
+        PageIndex: page,
+        PageSize: per
+      ).tap { |params| params[:Signature] = sign(params) }
+      connection.post("/", request_params)
+        .tap { |resp| AliyunSms.logger.info "#{self.class.name} query_template_list response(#{resp.status}): #{resp.body.squish}" }
+        .then { |resp| JSON.parse(resp.body) }
     end
 
     def sign(sign_params, http_method = "POST")
@@ -46,6 +80,19 @@ module AliyunSms
             "Content-Type": "application/x-www-form-urlencoded",
           }.merge(extra_headers).compact
         )
+      end
+
+      def common_params(options: {})
+        {
+          AccessKeyId: access_key_id,
+          Timestamp: Time.now.utc.strftime("%FT%TZ"),
+          SignatureMethod: "HMAC-SHA1",
+          SignatureVersion: "1.0",
+          SignatureNonce: Time.now.utc.strftime("%Y%m%d%H%M%S%L"),
+          Version: "2017-05-25",
+          Format: "JSON",
+          RegionId: (options[:region_id] || "cn-hangzhou")
+        }
       end
   end
 end
